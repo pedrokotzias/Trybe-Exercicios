c0a3fe00bd89cae744105b7eb169f8b44e5137ad
Pedro Kotzias <pepeuk@gmail.com>
2022-05-31 18:47:48 -0300

    exercio 4 e 5 feitos

--- fundamentos/bloco-04-introdução-javascript-logica-programacao/dia-02-array-loop-for/exercicios4-2.js
+++ fundamentos/bloco-04-introdução-javascript-logica-programacao/dia-02-array-loop-for/exercicios4-2.js
@@ -7,12 +7,12 @@ for (let i = 0; i < numbers.length; i += 1) {
 
 // exercicio 2
 let sum = 0;
-for (let index = 0; index < numbers.length; index += 1) {
-  sum += numbers[index];
+for (let index2 = 0; index2 < numbers.length; index2 += 1) {
+  sum += numbers[index2];
 }
 console.log(sum);
 
-// exercicio 3
+// exercicio 3 e 4
 let sum3 = 0;
 let average = 0;
 for (let index3 = 0; index3 < numbers.length; index3 += 1) {
@@ -20,4 +20,19 @@ for (let index3 = 0; index3 < numbers.length; index3 += 1) {
   average += 1;
 
 }
+if (sum3/average > 20) {
+  console.log("valor maior que 20")
+} else {
+  console.log("valor menor ou igual a 20")
+}
 console.log(sum3/average);
+
+// exercicio 5
+let compare = numbers[0];
+
+for (let index = 1; index < numbers.length; index += 1) {
+  if (numbers[index] > compare) {
+    compare = numbers[index];
+  }
+}
+console.log(compare);
