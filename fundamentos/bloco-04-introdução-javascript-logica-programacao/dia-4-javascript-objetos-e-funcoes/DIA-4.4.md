3597d823dbf2ad4762198165413d76afb00f999d
Pedro Kotzias <pepeuk@gmail.com>
2022-06-02 15:13:10 -0300

    adicionando conteudo

--- fundamentos/bloco-04-introdução-javascript-logica-programacao/dia-4-javascript-objetos-e-funcoes/DIA-4.4.md
+++ fundamentos/bloco-04-introdução-javascript-logica-programacao/dia-4-javascript-objetos-e-funcoes/DIA-4.4.md
@@ -1,1 +1,8 @@
 #DIA 4.4
+
+Nesse dia aprendemos:
+
+    Manipular objetos;
+    Utilizar os comandos for/in e for/of;
+    Utilizar funções para organizar e estruturar o seu código;
+
---
+++ fundamentos/bloco-04-introdução-javascript-logica-programacao/dia-4-javascript-objetos-e-funcoes/praticaconteudo.js
@@ -0,0 +1,35 @@
+// objetos
+let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: { golden: 2, silver: 3 },
+    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
+  };
+
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade');
+
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' vezes');
+
+console.log('A jogadora possui ' + player.medals['golden'] + ' medalhas de ouro e ' + player.medals['silver'] + ' medalhas de prata');
+
+// for/in e for/of
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge',
+};
+
+for (let index in names) {
+  console.log('Olá ' + names[index]);
+}
+
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+
+for (let index in car) {
+  console.log(index + ': ' + car[index]);
+}
