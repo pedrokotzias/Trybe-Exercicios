e893034fafb0c06c9485c95305d237ef37f8f7b5
Pedro Kotzias <pepeuk@gmail.com>
2022-06-24 17:26:46 -0300

    arrumando exercicio 1

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicios-7.3/sum.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicios-7.3/sum.test.js
@@ -1,21 +1,12 @@
 const sum = require('./sum.js');
 
-test('sums 4 plust 5', () => {
-  expect(sum(4, 5)).toEqual(9);
-});
-
-test('sums zero plus zero', () => {
-  expect(sum(0, 0)).toEqual(0);
-});
-
-test('sums zero plus zero', () => {
-  expect(sum(0, 0)).toEqual(0);
-});
-
-test('check if throws an error if any of the parameters isnt a number', () => {
-  expect(() => {sum() }).toThrow();
-});
-
-test('check if the error message is "parameters must be numbers"', () => {
-  expect(() => { sum() }).toThrowError(new Error('parameters must be numbersq'));
+describe('testing sum function', () => {
+  it('return sum result', () => {
+    expect(sum(4, 5)).toBe(9);
+    expect(sum(0, 0)).toBe(0);
+  });
+  it('check if throws an error if any of the parameters isnt a number and shows error message', () => {
+    expect(() => {sum(4, '5') }).toThrow();
+    expect(() => { sum(4, '5') }).toThrowError('parameters must be numbers');
+  });
 });
