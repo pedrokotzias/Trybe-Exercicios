07736ef7c85ddb0fdec92892524cc4556d7acad9
Pedro Kotzias <pepeuk@gmail.com>
2022-10-06 16:58:10 -0300

    organizando github

---
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-inputs/fixation-exercises/src/App.test.js
@@ -0,0 +1,45 @@
+// App.test.js
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+afterEach(() => jest.clearAllMocks());
+
+it('fetches a joke', async () => {
+  const joke = {
+    id: '7h3oGtrOfxc',
+    joke: 'Whiteboards ... are remarkable.',
+    status: 200,
+  };
+
+  jest.spyOn(global, 'fetch');
+  global.fetch.mockResolvedValue({
+    json: jest.fn().mockResolvedValue(joke),
+  });
+
+  render(<App />);
+  const renderedJoke = await screen.findByText('Whiteboards ... are remarkable.');
+  expect(renderedJoke).toBeInTheDocument();
+  expect(global.fetch).toHaveBeenCalledTimes(1);
+  expect(global.fetch).toHaveBeenCalledWith(
+    'https://icanhazdadjoke.com/',
+    { headers: { Accept: 'application/json' } },
+  );
+});
+it('fetches a new joke when button is clicked', async () => {
+  const joke1 = {
+    id: '7h3oGtrOfxc',
+    joke: 'Whiteboards ... are remarkable.',
+    status: 200,
+  };
+
+  jest.spyOn(global, 'fetch');
+  global.fetch.mockResolvedValue({
+    json: jest.fn().mockResolvedValue(joke1),
+  });
+
+  render(<App />);
+
+  expect(await screen.findByText(joke1.joke)).toBeInTheDocument();
+  expect(global.fetch).toBeCalledTimes(1);
+});
